1ace2767724efdd5735505194d444c95cdfaaa80
李昕 <lixin.henry@bytedance.com>
2024-09-18 19:14:47 +0800

    feat: update TestScene.h

--- src/ui/scenes/TestScene.h
+++ src/ui/scenes/TestScene.h
@@ -7,19 +7,27 @@
 
 class TestScene : public Scene {
  protected:
-  void onInit() {
+  lv_obj_t *label;
+
+  void onInit() override {
     Scene::onInit();
 
     auto img = lv_img_create(root);
-    mx_img(img, "A:/image.jpg");
+    mx_img(img, "A:/watch_face_001.jpg");
     mx_center(img);
 
-    auto label = lv_label_create(root);
+    label = lv_label_create(root);
     mx_text_color(label, 0x520404);
 
     mx_text(label, "19:38");
     mx_center_x(label);
     mx_top(label, 44);
     mx_font(label, 72);
+  }
+
+  void onUpdate() override {
+    Scene::onUpdate();
+
+    mx_text_fmt(label, "%d", millis() / 1000);
   }
 };
