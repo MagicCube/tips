8f829812866eaa391a5f8413a2d164727bccb9e9
李昕 <lixin.henry@bytedance.com>
2024-09-21 15:41:21 +0800

    feat: add a spinner

--- src/ui/scenes/WiFiConnectScene.h
+++ src/ui/scenes/WiFiConnectScene.h
@@ -8,6 +8,7 @@
 class WiFiConnectScene : public Scene {
  protected:
   MXObject *label;
+  MXObject *spinner;
 
   void onInit() override {
     Scene::onInit();
@@ -20,7 +21,12 @@ class WiFiConnectScene : public Scene {
                  .center()
                  .font(&lv_font_montserrat_24);
 
-    mx(this)->spinner();
+    spinner = &mx(this)->spinner();
+  }
+
+  void onActivate() override {
+    Scene::onActivate();
+    spinner->show();
   }
 
   void onUpdate() override {
@@ -28,6 +34,23 @@ class WiFiConnectScene : public Scene {
     auto connection = WiFiConnection::singleton();
     if (connection->isConnected()) {
       label->text("Connected");
+
+      if (spinner) {
+        spinner->hide();
+        delete spinner;
+        spinner = nullptr;
+      }
+
+      auto arc = lv_arc_create(root->lv_obj());
+      lv_arc_set_rotation(arc, 270);
+      lv_arc_set_bg_angles(arc, 0, 360);
+      lv_arc_set_value(arc, 100);
+      lv_obj_set_size(arc, width, height);
+      lv_obj_center(arc);
+      lv_obj_set_style_arc_color(arc, lv_color_hex(0x00ff00),
+                                 LV_PART_INDICATOR);
+      lv_obj_remove_style(arc, NULL, LV_PART_KNOB);
+      lv_obj_clear_flag(arc, LV_OBJ_FLAG_CLICKABLE);
     }
   }
 };
